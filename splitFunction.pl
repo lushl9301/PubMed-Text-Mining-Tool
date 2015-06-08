e1b5504338d39ab28291675f771bbacfce8ebb85
lushl9301 <lushl9301@gmail.com>
2015-06-08 15:51:35 +0800

    minor change

--- splitFunction.pl
+++ splitFunction.pl
@@ -3,7 +3,7 @@ sub _split {
     my $tempAbstract = $_[0];
     chomp($tempAbstract);
     my $splitted = get_sentences($tempAbstract);
-    my $abstractSentences = join("\n", @$splitted);
+    my $abstractSentences = join("\n", @$splitted) . "\n";
     my $counter = @$splitted;
     return ($counter, $abstractSentences)
 }

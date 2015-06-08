c6fb6f391a432cb091749d64eda472332d6015e2
lushl9301 <lushl9301@gmail.com>
2015-06-08 15:38:18 +0800

    jsonParser for extract result from pyspider sqlite

---
+++ jsonParser.pl
@@ -0,0 +1,29 @@
+use warnings;
+use Lingua::EN::Sentence qw(get_sentences);
+use Unicode::Normalize 'normalize';
+use JSON;
+
+require "splitFunction.pl";
+
+binmode(STDOUT, ":utf8");
+
+$filename = "raw_data.json";
+open FILE, "<:encoding(utf-8)", $filename or die "can't find file ".$filename;
+while ($readinline = <FILE>) {
+    if ($readinline =~ /^\n/) {
+        next;
+    }
+    $result = decode_json($readinline);
+    $pmid = "PMID- 88888888\n";
+    $title = "Title- " . $result->{'title'};
+    $abstract = "Abstract- " . $result->{'abstract'};
+
+    if ($title =~ /Title- \[/ or $abstract =~ /^.{7,12}$/) { #if not english or no abstract, ignore
+        next;
+    }
+    ($counter, $abstractSentences) = _split($abstract);
+    
+    if ($counter > 0) {
+        print "$pmid" . "$title\n" . $abstractSentences . "\n";
+    }
+}

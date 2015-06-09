69a283b68bb93db7cae94ab74ca8134da75aad47
lushl9301 <lushl9301@gmail.com>
2015-06-09 11:02:59 +0800

    deal with different type of raw data

--- nextStep.py
+++ nextStep.py
@@ -1,17 +1,40 @@
+import json
+
 pmidList = "".join(open("[3]pmidList.txt", 'r').readlines()).split('\n')
 pmidList.pop(); #remove last one which is ''
-pmidIDOnlyList = [];
+urlOnlyList = [];
 for pmid in pmidList:
-    _pmid, title = pmid.split('#')
-    pmidIDOnlyList.append(_pmid[:-1])
+    _url, title = pmid.split('!!')
+    urlOnlyList.append(_url[:-1])
 
 index = 0
-with open("pubmed_result.txt", "r") as myFormat:
-    while index < len(pmidIDOnlyList):
-        content = myFormat.readline()
-        if len(content) < 17 and content[:-1] == pmidIDOnlyList[index]:
-            while content != "\n":
-                print content[:-1]
-                content = myFormat.readline()
+length = len(urlOnlyList)
+try:
+    with open("pubmed_result.txt", "r") as myFormat:
+        outputFile = open("new_pubmed_result.txt", "w")
+        while index < length:
+            content = myFormat.readline()
+            if content == "":
+                break
+            pmid = urlOnlyList[index].split("/")[-1];
+            if len(content) < 17 and pmid == content[6:-1]:
+                while content != "\n" and content != "":
+                    outputFile.write(content)
+                    content = myFormat.readline()
+                index = index + 1
+                outputFile.write("\n")
+        outputFile.close()
+except:
+    pass
+with open("raw_data.json", "r") as myFormat:
+    outputFile = open("new_raw_data.json", "w")
+    while index < length:
+        jsonString = myFormat.readline()
+        if jsonString == "":
+            break
+        s = json.loads(jsonString)
+        url = urlOnlyList[index][5:];
+        if str(s['url']) == url:
+            outputFile.write(jsonString)
             index = index + 1
-            print
+    outputFile.close()

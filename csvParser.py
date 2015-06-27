40707c276c951398433da0df965dcd5e32983680
lushl9301 <lushl9301@gmail.com>
2015-06-27 14:51:40 +0800

    csv python parser

---
+++ csvParser.py
@@ -0,0 +1,17 @@
+import csv
+import json
+
+
+jsonFile = open("raw_data.json", "a")
+with open('export.csv') as csvFile:
+    csvFile.readline()
+    fieldNames = csvFile.readline().replace('\n','').split(',')
+    reader = csv.DictReader(csvFile, fieldNames)
+    for row in reader:
+        tempDict = {}
+        tempDict["url"] = row['PDF Link']
+        tempDict["abstract"] = row['"Abstract"']
+        tempDict["authors"] = [row['Authors']]
+        tempDict["title"] = row['"Document Title"']
+        json.dump(tempDict, jsonFile)
+        jsonFile.write("\n")

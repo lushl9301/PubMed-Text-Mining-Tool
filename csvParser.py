8ae94254643e6d923bc84897ec0c8939fe2ddf98
lushl9301 <lushl9301@gmail.com>
2015-06-27 15:22:23 +0800

    temp file to make sure no error

--- csvParser.py
+++ csvParser.py
@@ -1,18 +1,25 @@
 import csv
 import json
+import sys
 
-
-jsonFile = open("raw_data.json", "a")
-jsonFile.write("\n")
-with open('export.csv') as csvFile:
-    csvFile.readline()
-    fieldNames = csvFile.readline().replace('\n','').split(',')
-    reader = csv.DictReader(csvFile, fieldNames)
-    for row in reader:
-        tempDict = {}
-        tempDict["url"] = row['PDF Link']
-        tempDict["abstract"] = row['"Abstract"']
-        tempDict["authors"] = [row['Authors']]
-        tempDict["title"] = row['"Document Title"']
-        json.dump(tempDict, jsonFile)
-        jsonFile.write("\n")
+jsonFile = open('temp.json', "w")
+try:
+    with open('export.csv') as csvFile:
+        csvFile.readline()
+        fieldNames = csvFile.readline().replace('\n','').split(',')
+        reader = csv.DictReader(csvFile, fieldNames)
+        for row in reader:
+            jsonFile.write("\n")
+            tempDict = {}
+            tempDict["url"] = row['PDF Link']
+            tempDict["abstract"] = row['"Abstract"']
+            tempDict["authors"] = [row['Authors']]
+            tempDict["title"] = row['"Document Title"']
+            json.dump(tempDict, jsonFile)
+except:
+    print "Error: only support csv file provided by ieee xplore"
+    sys.exit()
+jsonFile.close()
+jsonFile = open('temp.json', "r")
+with open('111.json', "a") as dataFile:
+    dataFile.write(jsonFile.read())
